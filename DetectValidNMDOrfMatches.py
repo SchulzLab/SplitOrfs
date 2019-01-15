747d6dfd3784ea392b4d4501c54daf67864375fd
SchulzLab <marcel.schulz@iscb.org>
2019-01-15 17:54:14 +0100

    modified detectoin script

--- DetectValidNMDOrfMatches.py
+++ DetectValidNMDOrfMatches.py
@@ -19,13 +19,27 @@ def checkAlignments(Alignments,gene, target) :
                     for match in Alignments:
                         entries = Alignments[match]
                         if len(entries) > 1: #found more than one ORF 
+                            #compute max and min sequence identity values
+                            MinSeqIdent=float(100)
+                            MaxSeqIdent=float(0)
+
                             orfIDs =[]
                             orfPos =[]
+                            orfSeqIdents = []
+                            orfLengths = []
                             for i in range(0,len(entries)) :    
                                 orfIDs.append(entries[i].split(":")[0])
                                 orfPos.append("-".join([entries[i].split(":")[1],entries[i].split(":")[2]]))
+                                currentSeqIdent=float(entries[i].split(":")[3])
+                                if(currentSeqIdent < float(MinSeqIdent)) :
+                                    MinSeqIdent = currentSeqIdent
+                                if(currentSeqIdent > MaxSeqIdent) :
+                                    MaxSeqIdent = float(currentSeqIdent)
+                                orfSeqIdents.append(str(currentSeqIdent))
+                                orfLengths.append(str(int(entries[i].split(":")[2])-int(entries[i].split(":")[1])+1))
 
-                            print "\t".join([gene,target,match,str(len(entries)),",".join(orfIDs),",".join(orfPos)])
+
+                            print "\t".join([gene,target,match,str(len(entries)),",".join(orfIDs),",".join(orfPos),",".join(orfLengths),",".join(orfSeqIdents),str(MinSeqIdent),str(MaxSeqIdent)])
 
 #read in fasta file
 if len(sys.argv) < 2:
@@ -37,7 +51,7 @@ else :
         lastElem="dummy" #variable that remembers the last transcript in use (all alignments are sorted by traget sequence)
         Alignments = {}  #hash map that stores all valid alignments for one target sequence
         #print header for result file
-        print "\t".join(["geneID", "targetTransID" ,"OrfTransID", "NumOrfs","OrfIDs","OrfPos"])
+        print "\t".join(["geneID", "targetTransID" ,"OrfTransID", "NumOrfs","OrfIDs","OrfPos","OrfLengths","OrfSeqIdents","MinSeqIdent","MaxSeqIdent"])
 
         for line in file :
             elems = line.split("\t")
@@ -51,10 +65,12 @@ else :
                 Alignments={}
 
             if float(elems[2]) >= identityCutoff and (orf[0] == target[0]):
+                dummy=orf[2:5]
+                dummy.append(str(elems[2]))
                 if(orf[1] in Alignments) :  #transcript has at least one matching orf
-                    Alignments[orf[1]].append(colon.join(orf[2:5]))
+                    Alignments[orf[1]].append(colon.join(dummy))
                 else :
-                    Alignments[orf[1]] = [colon.join(orf[2:5])]
+                    Alignments[orf[1]] = [colon.join(dummy)]
             
         checkAlignments(Alignments,target[0],target[1])
 
